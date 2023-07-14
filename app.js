221beb501c952a8a762669c558688798bf452d1c
NikoSula <niko.sulaqvelidze@gmail.com>
2023-07-15 00:28:31 +0400

    add app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+let averageWeight = 88.6;
+let isEmployed = true;
+let isStudent = false;
+let userEmail = "Niko.sulaqvelidze@gmail.com";
+let myName = "Niko";
+let myHobby = "playing guitar";
+let yearOfBirth = 1995;
+let currentYear = 2023;
+let substraction = currentYear - yearOfBirth ;
+
+console.log("My name is",myName ,"," ,"I am" ,substraction ,"years old and my hobby is" ,myHobby);
+
+console.log(`My name is ${myName}, I am ${substraction} years old and my hobby is ${myHobby}`)
